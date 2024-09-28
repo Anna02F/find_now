e17ca7202e403729f44806b9f280147af7267513
Anna Aghajanyan <anna.aghajanyan987@gmail.com>
2024-09-28 13:25:26 -0700

    Set up the component structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Grid2 } from "@mui/material";
+
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid2 container spacing={3} style={{ width: "100%" }}></Grid2>
+    </>
   );
 };
 
