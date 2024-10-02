c9ee0bb9c344c684fb6d5d287fb85ed8d1ed7543
Anna Aghajanyan <anna.aghajanyan987@gmail.com>
2024-10-01 20:47:40 -0700

    Set up the google map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import googleMapReact from "google-map-react";
+import {
+  useTheme,
+  Paper,
+  Typography,
+  useMediaQuery,
+  Rating,
+} from "@mui/material";
+import PlaceOutlinedIcon from "@mui/icons-material/PlaceOutlined";
 
 const Map = () => {
-  return <div>Map</div>;
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.up("sm"));
+  const coordinates = { lat: 0, lng: 0 };
+  return (
+    <Box sx={{ height: "100vh", width: "100%" }}>
+      <googleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={11}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></googleMapReact>
+    </Box>
+  );
 };
 
 export default Map;
