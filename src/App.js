c5ed8e0b34d0fa00bfa4671e8950977ff68546d1
Anna Aghajanyan <anna.aghajanyan987@gmail.com>
2024-10-02 20:36:19 -0700

    Add Category and Rating selects to List component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Grid2 } from "@mui/material";
+import { Grid2, Box } from "@mui/material";
 
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
@@ -9,17 +9,17 @@ import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
 
 const App = () => {
   return (
-    <>
+    <Box sx={{ padding: "0 0.5rem 0.5rem 1.5rem" }}>
       <Header />
       <Grid2 container spacing={3} sx={{ width: "100%" }}>
-        <Grid2 size={{ xs: 12, md: 4 }} sx ={{pt: '5rem'}}>
+        <Grid2 size={{ xs: 12, md: 4 }} sx={{ pt: "5rem" }}>
           <List />
         </Grid2>
-        <Grid2 size={{ xs: 12, md: 8 }} sx ={{pt: '5rem'}}>
+        <Grid2 size={{ xs: 12, md: 8 }} sx={{ pt: "5rem" }}>
           <Map />
         </Grid2>
       </Grid2>
-    </>
+    </Box>
   );
 };
 
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Box,
+  CircularProgress,
+  Grid2,
+  Typography,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from "@mui/material";
 
 const List = () => {
-  return <div>List</div>;
+  const [category, setCategory] = useState("restaurants");
+  const [rating, setRating] = useState(0);
+  return (
+    <Box>
+      <Typography variant="h4">
+        Nearby Restaurants, Hotels and Attractions
+      </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          width: "15rem",
+          justifyContent: "space-between",
+          marginTop: "1.5rem",
+        }}
+      >
+        <FormControl sx={{ maxwidth: "50%", minwidth: "fit-content" }}>
+          <InputLabel id="category-select-label">Select Category</InputLabel>
+          <Select
+            labelId="category-select-label"
+            id="category-select"
+            value={category}
+            label="Select Category"
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <MenuItem value="restaurants">Restaurants</MenuItem>
+            <MenuItem value="hotels">Hotels</MenuItem>
+            <MenuItem value="attractions">Attractions</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl sx={{ maxWidth: "20%", minWidth: "6rem" }}>
+          <InputLabel id="rating-select-label">Select Rating</InputLabel>
+          <Select
+            labelId="rating-select-label"
+            id="rating-select"
+            value={rating}
+            label="Select Rating"
+            onChange={(e) => setRating(e.target.value)}
+          >
+            <MenuItem value={0}>All</MenuItem>
+            <MenuItem value={3}>Above 3.0</MenuItem>
+            <MenuItem value={4}>Above 4.0</MenuItem>
+            <MenuItem value={4.5}>Above 4.5</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+    </Box>
+  );
 };
 
 export default List;
